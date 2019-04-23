944ae51ad75923d83b1eeb6ee97d05e7603117a2
Theo Weller <weller.theodor@gmail.com>
2019-04-23 22:15:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function produceDrivingRange(blockRange) {
+  return function(arg1, arg2) {
+    arg1 = arg1.replace(/\D/g,'')
+    arg1 = Number(arg1)
+    arg2 = arg2.replace(/\D/g,'')
+    arg2 = Number(arg2)
+
+    let range = arg2 - ar1;
+    let inRange = blockRange - range;
+    let outOfRange = range - blockRange
+
+    if (range <= blockRange) {
+      return 'within range by ${inRange}';
+    } else {
+      return '${outOfRange} blocks out of range'
+    };
+
+
+  }
+}
+
+// const tenBlockRange = produceDrivingRange(10)
+// const fiveBlockRange = produceDriving(5)
