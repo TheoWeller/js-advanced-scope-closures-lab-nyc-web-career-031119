f2602be216395ae408a67c5aaf311367a7847785
Theo Weller <weller.theodor@gmail.com>
2019-04-23 22:36:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ function produceTipCalculator(percent) {
 
 function createDriver() {
   class Driver {
+    let id = 0;
+    {id++}
     constructor(name, id) {
       this.name = name;
       this.id = id;
     }
 
-    function(id) {
-      this.id = id++;
-    }
+
   }
   return Driver;
 }
